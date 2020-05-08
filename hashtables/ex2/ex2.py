5a150072a225dc9af21da470ea88a558ac39da09
fishmanjohn <john@emmaandjohnbyvan.com>
2020-05-08 09:44:27 -0700

    ex2 down

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,4 +1,6 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
+
+
 class Ticket:
     def __init__(self, source, destination):
         self.source = source
@@ -6,9 +8,19 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    
-    """
-    YOUR CODE HERE
-    """
-
+    #initialize dictionary
+    ticket_dict = {}
+    #create list of nones in the proper length
+    route = [None] * (len(tickets))
+    #iterate over tickets list
+    for ticket in tickets:
+        # if ticket sorce is none move to head of route list this is the starting location
+        if ticket.source == "NONE":
+            route[0] = ticket.destination
+        #add not none tickets to ticket dict
+        ticket_dict[ticket.source] = ticket.destination
+    # add ticket dict k/v to route
+    for i in range(1, len(tickets)):
+        route[i] = ticket_dict[route[i-1]]
+    # return route
     return route
